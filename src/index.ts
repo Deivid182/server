9ed2e9bb2d952cef78d925fc0af8a7e0e7491798
Deivid182 <davidmendoza911119@gmail.com>
2024-03-04 00:41:49 -0600

    edit cors options

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
 
 app.use("/api/auth", authRouter);
 app.use("/api/users", usersRouter);
--- src/middleware/validate-auth.ts
+++ src/middleware/validate-auth.ts
@@ -9,13 +9,14 @@ declare global {
   }
 }
 export const validateAuth = (req: Request, res: Response, next: NextFunction) => {
-
+  
   if (!req.cookies.token) {
     return res.status(401).json({ message: "Unauthorized" })
   }
   try {
     const decoded = verify(req.cookies.token, process.env.TOKEN_SECRET!)
     req.userId = (decoded as JwtPayload).userId
+    next()
   } catch (error) {
     return res.status(401).json({ message: "Unauthorized" })
   }
